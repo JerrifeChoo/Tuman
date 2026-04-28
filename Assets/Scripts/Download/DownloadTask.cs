fd44c76c7880f6d6a463dafdca1a63f6397349be
Page7\Tutu <jerrife.choo@foxmail.com>
2026-04-28 15:26:56 +0800

    删除无用定义

--- Assets/Scripts/Download/DownloadTask.cs
+++ Assets/Scripts/Download/DownloadTask.cs
@@ -49,33 +49,10 @@ namespace TT.Download {
         }
     }
 
-    //public enum DownloadStatus
-    //{
-    //    Pending,
-    //    Downloading,
-    //    Success,
-    //    Failed,
-    //    Cancelled
-    //}
 
     public struct FileBin
     {
         public long[] Downloads;
     }
 
-    //public class DownloadResult
-    //{
-    //    public string Url { get; set; }
-    //    public string FilePath { get; set; }
-    //    public bool Success { get; set; }
-    //    public string Error { get; set; }
-    //}
-
-    //public class DownloadProgress
-    //{
-    //    public string Url { get; set; }
-    //    public float Progress { get; set; }
-    //    public int CompletedCount { get; set; }
-    //    public int TotalCount { get; set; }
-    //}
 }
